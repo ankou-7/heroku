81cc764cf393c95a4efde16578c3276a064d53b9
安江洸希 <yasuekouki@ankou.local>
2020-02-21 04:02:41 +0900

    initial commit

--- run/patarn_match.py
+++ run/patarn_match.py
@@ -9,23 +9,23 @@ Created on Fri Feb 21 03:04:00 2020
 import re
 
 def titlename(list):
-titlelist=[];
-for i in range(len(list)):
-    count=0;
-    flag=0;
-    for m in re.finditer('"', list[i]):
-        title = m.span()
-        count=count+1;
-        if count==5:
-            title_first=title[0];
-            flag=1;
-        elif count==6:
-            title_end=title[0];
-    if flag==1:
-        titlelist.append(list[i][title_first+1:title_end]);
+    titlelist=[];
+    for i in range(len(list)):
+        count=0;
         flag=0;
+        for m in re.finditer('"', list[i]):
+            title = m.span()
+            count=count+1;
+            if count==5:
+                title_first=title[0];
+                flag=1;
+            elif count==6:
+                title_end=title[0];
+        if flag==1:
+            titlelist.append(list[i][title_first+1:title_end]);
+            flag=0;
 
-return titlelist;
+    return titlelist;
 
 def make_title():
     kiji1='wikimanga.txt'
