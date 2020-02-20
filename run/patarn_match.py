c953311c89da9aac87247a9c22bc63e27ff88f01
安江洸希 <yasuekouki@ankou.local>
2020-02-21 03:54:43 +0900

    initial commit

--- run/patarn_match.py
+++ run/patarn_match.py
@@ -7,7 +7,25 @@ Created on Fri Feb 21 03:04:00 2020
 """
 
 import re
-from method import kuuhakujokyo, titlename
+
+def titlename(list):
+titlelist=[];
+for i in range(len(list)):
+    count=0;
+    flag=0;
+    for m in re.finditer('"', list[i]):
+        title = m.span()
+        count=count+1;
+        if count==5:
+            title_first=title[0];
+            flag=1;
+        elif count==6:
+            title_end=title[0];
+    if flag==1:
+        titlelist.append(list[i][title_first+1:title_end]);
+        flag=0;
+
+return titlelist;
 
 def make_title():
     kiji1='wikimanga.txt'
@@ -17,12 +35,3 @@ def make_title():
     title=titlename(list)
     return title
 
-if __name__ == "__main__":
-    
-    kiji1='wikimanga.txt'
-    f = open(kiji1,'r',encoding='utf-8')
-    list = f.read().split('</doc>')# ファイル終端まで全て読んだデータを返す
-    f.close()
-    bunlist=kuuhakujokyo(re.split('[\n。\t]', list[664])) #記事を文ごとに分割
-    
-    title2=make_title()
