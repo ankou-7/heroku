3879c63b8dcb7343795380938203c3933bb46fb0
安江洸希 <yasuekouki@ankou.local>
2020-02-23 01:06:16 +0900

    add new file

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+wikipedia.set_lang("ja") # 追加
+
 @app.route("/callback", methods=['POST'])
 def callback():
    # get X-Line-Signature header value
